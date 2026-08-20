f3801f3fadd1038851f2752890b739a1671678f2
pedrohcnascimento <1.77986617e+08+pedrohcnascimento@users.noreply.github.com>
2026-08-20 18:57:38 +0000

    feat: inicializa projeto com Spring Boot

---
+++ src/main/java/org/phcn/DesafioBancoApplication.java
@@ -0,0 +1,12 @@
+package org.phcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBancoApplication.class, args);
+    }
+}
